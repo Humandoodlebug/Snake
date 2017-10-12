5cecfe266659a2accb30991473532dac60ee971a
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2017-10-12 20:28:15 +0100

    .

--- Snake/SnakeWindows/Program.cs
+++ Snake/SnakeWindows/Program.cs
@@ -15,6 +15,16 @@ namespace SnakeWindows
         static void Main(string[] args)
         {
             Direction direction;
+
+            //Initialise the snake grid
+            int[,] snakeGrid = new int[15,15];
+            for (int i = 0; i < 15; i++)
+            { for(int j = 0; j < 15; j++)
+                {
+                    snakeGrid[i,j] = 0;
+                }
+            }
+
             while (true)
             {
                 if (Console.KeyAvailable)
@@ -43,6 +53,34 @@ namespace SnakeWindows
                 if (Environment.TickCount % 400 == 0)
                 {
                     //Move snake
+                    for (int i = 0; i < 15; i++) { 
+                        for(int j = 0; j < 15; j++) {
+                            
+                            
+                        }
+                    }
+                    
+                    //Initialise the length to 3.
+                    snakeGrid[7,7] = 3;
+                    snakeGrid[7,6] = 2;
+                    snakeGrid[7,5] = 1;
+
+                    //Update the snake grid
+                    for (int i = 0; i < 15; i++)
+                    { for(int j = 0; j < 15; j++)
+                        {
+                            //Console.Write(snakeGrid[i,j]);
+                            //Format this properly
+                            if (snakeGrid[i,j] > 0) {
+                                Console.Write("+");
+                            } else if (snakeGrid[i,j] < 0) {
+                                Console.Write("x");
+                            } else {
+                                Console.Write(".");
+                            }
+                        }
+                        Console.Write("\n");
+                    }
                 }
             }
         }
