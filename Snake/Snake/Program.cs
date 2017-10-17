a1388ded27a45909e0b0981347eb4d29a90d0e43
Samuel Collins <samuel.collins@live.co.uk>
2017-10-17 16:31:35 +0100

    .

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,61 +1,151 @@
 ﻿using System;
-using System.Threading;
-using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Snake
+namespace SnakeWindows
 {
+
     class Program
     {
-        
+
+        enum Direction { Right, Down, Up, Left };
+
+        static (int row, int column) GetRandomFoodPosition(int[,] grid)
+        {
+            Random random = new Random();
+            (int row, int column) pos;
+            do
+            {
+                pos.row = random.Next(grid.GetLength(0));
+                pos.column = random.Next(grid.GetLength(1));
+            }
+            while (grid[pos.row, pos.column] != 0);
+            return pos;
+        }
 
         static void Main(string[] args)
         {
-            var key = Console.L
+            Direction direction = default(Direction);
+            int snakeLength = 3;
 
+
+            //Initialise the snake grid
+            int[,] snakeGrid = new int[15, 15];
+            //Initialise the length to 3.
+            snakeGrid[8, 8] = 3;
+            snakeGrid[8, 7] = 2;
+            snakeGrid[8, 6] = 1;
+            var preTickCount = 0;
+
+            (int row, int column) snakePosition = (8, 8);
+            (int row, int column) foodPosition = GetRandomFoodPosition(snakeGrid);
             while (true)
             {
-
-
-                if (Environment.TickCount % 400 == 0)
+                if (Console.KeyAvailable)
                 {
-                    //Move snake
-
-                    //Initialise the snake grid
-                    int[,] snakeGrid = new int[15,15];
-                    for (int i = 0; i < 15; i++)
-                    { for(int j = 0; j < 15; j++)
-                        {
-                            snakeGrid[i,j] = 0;
-                        }
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.RightArrow:
+                            direction = Direction.Right;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            direction = Direction.Down;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            direction = Direction.Left;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            direction = Direction.Up;
+                            break;
+                        case ConsoleKey.Escape:
+                            Environment.Exit(0);
+                            break;
                     }
-                    
-                    //Initialise the length to 3.
-                    snakeGrid[7,7] = 3;
-                    snakeGrid[7,6] = 2;
-                    snakeGrid[7,5] = 1;
+                }
 
-                    //Update the snake grid
+
+
+                if (Environment.TickCount % 60 < 2)
+                {
+                    //Draw board
+                    switch (direction)
+                    {
+                        case Direction.Right:
+                            snakePosition.column++;
+                            break;
+                        case Direction.Down:
+                            snakePosition.row++;
+                            break;
+                        case Direction.Left:
+                            snakePosition.column--;
+                            break;
+                        case Direction.Up:
+                            snakePosition.row--;
+                            break;
+
+                    }
+                    //Detecting snake collisions
+                    if (snakePosition.row < 0 || snakePosition.column >= snakeGrid.GetLength(1) || snakePosition.column < 0 || snakePosition.column >= snakeGrid.GetLength(1) || snakeGrid[snakePosition.row, snakePosition.column] > 0)
+                        break;
+
+
+                    //Moves snake tail
                     for (int i = 0; i < 15; i++)
-                    { for(int j = 0; j < 15; j++)
+                        for (int j = 0; j < 15; j++)
+                            if (snakeGrid[i, j] > 0)
+                                snakeGrid[i, j]--;
+
+
+                    if (snakePosition.row == foodPosition.row && snakePosition.column == foodPosition.column)
+                    {
+                        snakeLength++;
+                        foodPosition = GetRandomFoodPosition(snakeGrid);
+                    }
+
+                    snakeGrid[snakePosition.row, snakePosition.column] = snakeLength;
+
+
+                    //Display grid
+                    Console.Clear();
+                    for (int i = 0; i < snakeGrid.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < snakeGrid.GetLength(1); j++)
                         {
                             //Console.Write(snakeGrid[i,j]);
                             //Format this properly
-                            if (snakeGrid[i,j] > 0)
-                            {
-                                Console.Write("+");
-                            } else if (snakeGrid[i,j] < 0) {
-                                Console.Write("x");
-                            } else {
-                                Console.Write(" ");
-                            }
 
+                            if (i == snakePosition.row && j == snakePosition.column)
+                            {
+                                Console.Write(" O");
+                            }
+                            else if (snakeGrid[i, j] > 0)
+                            {
+                                Console.Write(" +");
+                            }
+                            else if (i == foodPosition.row && j == foodPosition.column)
+                            {
+                                Console.Write(" x");
+                            }
+                            else
+                            {
+                                Console.Write(" .");
+                            }
                         }
                         Console.Write("\n");
                     }
                 }
+
             }
+
+            Console.WriteLine("\n\nGame over...");
+
+            while (true)
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    Environment.Exit(0);
         }
 
-        static void KeyDown(object sender)
+
     }
 }
--- Snake/SnakeWindows/Program.cs
+++ Snake/SnakeWindows/Program.cs
@@ -9,24 +9,38 @@ namespace SnakeWindows
 
     class Program
     {
-        
+
         enum Direction { Right, Down, Up, Left };
+
+        static (int row, int column) GetRandomFoodPosition(int[,] grid)
+        {
+            Random random = new Random();
+            (int row, int column) pos;
+            do
+            {
+                pos.row = random.Next(grid.GetLength(0));
+                pos.column = random.Next(grid.GetLength(1));
+            }
+            while (grid[pos.row, pos.column] != 0);
+            return pos;
+        }
 
         static void Main(string[] args)
         {
-            Direction direction;
+            Direction direction = default(Direction);
             int snakeLength = 3;
 
+
             //Initialise the snake grid
-            int[,] snakeGrid = new int[15,15];
-            for (int i = 0; i < 15; i++)
-            { for(int j = 0; j < 15; j++)
-                {
-                    snakeGrid[i,j] = 0;
-                }
-            }
+            int[,] snakeGrid = new int[15, 15];
+            //Initialise the length to 3.
+            snakeGrid[8, 8] = 3;
+            snakeGrid[8, 7] = 2;
+            snakeGrid[8, 6] = 1;
+            var preTickCount = 0;
 
             (int row, int column) snakePosition = (8, 8);
+            (int row, int column) foodPosition = GetRandomFoodPosition(snakeGrid);
             while (true)
             {
                 if (Console.KeyAvailable)
@@ -51,59 +65,85 @@ namespace SnakeWindows
                     }
                 }
 
+                
 
-                if (Environment.TickCount % 400 == 0)
+                if (Environment.TickCount % 60 < 2)
                 {
                     //Draw board
-                    for (int i = 0; i < 15; i++) { 
-                        for(int j = 0; j < 15; j++) {
-                            switch (Console.ReadKey(true).Key)
-                        {
-                        case direction = Direction.Right:
-                            
+                    switch (direction)
+                    {
+                        case Direction.Right:
+                            snakePosition.column++;
                             break;
-                        case direction = Direction.Down:
+                        case Direction.Down:
+                            snakePosition.row++;
                             break;
-                        case direction = Direction.Left:
+                        case Direction.Left:
+                            snakePosition.column--;
                             break;
-                        case direction = Direction.Up:
+                        case Direction.Up:
+                            snakePosition.row--;
                             break;
-                        case ConsoleKey.Escape:
-                            Environment.Exit(0);
-                            break;
-                        }
-                        }
+
+                    }
+                    //Detecting snake collisions
+                    if (snakePosition.row < 0 || snakePosition.column >= snakeGrid.GetLength(1) || snakePosition.column < 0 || snakePosition.column >= snakeGrid.GetLength(1) || snakeGrid[snakePosition.row, snakePosition.column] > 0)
+                        break;
+
+
+                    //Moves snake tail
+                    for (int i = 0; i < 15; i++)
+                        for (int j = 0; j < 15; j++)
+                            if (snakeGrid[i, j] > 0)
+                                snakeGrid[i, j]--;
+
+
+                    if (snakePosition.row == foodPosition.row && snakePosition.column == foodPosition.column)
+                    {
+                        snakeLength++;
+                        foodPosition = GetRandomFoodPosition(snakeGrid);
                     }
 
-                    //Detecting snake collisions
-                    if 
-                    
-                    //Initialise the length to 3.
-                    snakeGrid[8,8] = 3;
-                    snakeGrid[8,7] = 2;
-                    snakeGrid[8,6] = 1;
+                    snakeGrid[snakePosition.row, snakePosition.column] = snakeLength;
 
-                    //Update the snake grid
-                    for (int i = 0; i < 15; i++)
-                    { for(int j = 0; j < 15; j++)
+
+                    //Display grid
+                    Console.Clear();
+                    for (int i = 0; i < snakeGrid.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < snakeGrid.GetLength(1); j++)
                         {
                             //Console.Write(snakeGrid[i,j]);
                             //Format this properly
-                            if (snakeGrid[i,j] > 0) {
-                                Console.Write("+");
-                            } else if (snakeGrid[i,j] < 0) {
-                                Console.Write("x");
-                            } else if (i == snakePosition.row && j == snakePosition.column) {
-                                Console.Write("O");
+
+                            if (i == snakePosition.row && j == snakePosition.column)
+                            {
+                                Console.Write(" O");
                             }
-                            } else {
-                                Console.Write(".");
+                            else if (snakeGrid[i, j] > 0)
+                            {
+                                Console.Write(" +");
+                            }
+                            else if (i == foodPosition.row && j == foodPosition.column)
+                            {
+                                Console.Write(" x");
+                            }
+                            else
+                            {
+                                Console.Write(" .");
                             }
                         }
                         Console.Write("\n");
                     }
                 }
+
             }
+
+            Console.WriteLine("\n\nGame over...");
+
+            while (true)
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    Environment.Exit(0);
         }
 
 
